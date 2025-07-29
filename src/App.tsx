d9acd4e4bab2c2946d9642e784effcdd24b578f5
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2025-07-29 17:19:23 -0300

    login de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
+import { AuthProvider } from './contexts/AuthContext';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
@@ -9,21 +10,23 @@ import Login from './pages/login/Login';
 function App() {
   return (
     <>
-    {/* ó, aqui dentro, vai rolar navegação */}
-      <BrowserRouter> 
-        <Navbar />
-        <div className="min-h-[80vh]">
-          {/* ó, aqui são as rotas unicas */}
-          <Routes>
-            {/* cada rota tem: Caminho (path), quem abre (element) */}
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        {/* ó, aqui dentro, vai rolar navegação */}
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            {/* ó, aqui são as rotas unicas */}
+            <Routes>
+              {/* cada rota tem: Caminho (path), quem abre (element) */}
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createContext, type ReactNode, useState } from "react"
+import { login } from "../services/Service"
+import type UsuarioLogin from "../model/UsuarioLogin"
+
+// tipagem das funcionalidades que eu vou querer no meu contexto, essa é a ultima parte a ser preenchida, de acordo com o que eu precisar no projeto
+interface AuthContextProps {
+    usuario: UsuarioLogin
+    handleLogout(): void
+    handleLogin(usuario: UsuarioLogin): Promise<void>
+    isLoading: boolean
+}
+
+// interface de tipagem padrão do context, esse cara sempre vai precisar estar aqui, desse jeito
+interface AuthProviderProps {
+    children: ReactNode
+}
+
+// criação do contexto em si, é uma const que sempre vai receber o "createContext({} as AuthContextProps)"
+export const AuthContext = createContext({} as AuthContextProps)
+
+// criação da função de provedor do context, sempre vai receber o "({ children }: AuthProviderProps)"
+export function AuthProvider({ children }: AuthProviderProps) {
+
+    // criação do estado que vai armazenar os dados do usuário que vai fazer o login
+    const [usuario, setUsuario] = useState<UsuarioLogin>({
+        id: 0,
+        nome: "",
+        usuario: "",
+        senha: "",
+        foto: "",
+        token: ""
+    })
+
+    // estado pra fazer o esqueminha de loading do botão
+    const [isLoading, setIsLoading] = useState(false)
+
+    // função que envia os dados do usuario para o backend, e se tudo der certo, da msg de ok, senão, da msg de erro
+    async function handleLogin(usuarioLogin: UsuarioLogin) {
+        setIsLoading(true)
+        try {
+            await login(`/usuarios/logar`, usuarioLogin, setUsuario)
+            alert("O Usuário foi autenticado com sucesso!")
+        } catch (error) {
+            alert("Os Dados do usuário estão inconsistentes!")
+        }
+        setIsLoading(false)
+    }
+
+    // função para deslogar o usuario, que simplesmente limpa todos os dados do estado, voltando ele pro estado original (sem token de acesso == não logado)
+    function handleLogout() {
+        setUsuario({
+            id: 0,
+            nome: "",
+            usuario: "",
+            senha: "",
+            foto: "",
+            token: ""
+        })
+    }
+
+    return (
+        <AuthContext.Provider value={{ usuario, handleLogin, handleLogout, isLoading }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
